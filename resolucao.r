666749a51e830feb055b50a1410837476989cc08
anakindinger <anakindinger@gmail.com>
2024-04-06 18:03:04 -0300

    treinamento e matriz de confusao RANDOM FOREST

--- resolucao.r
+++ resolucao.r
@@ -1,10 +1,27 @@
+library("mlbench")
+library("caret")
 
-data(Satellite)
-str(Satellite)
+
+data(Satellite) #carregamento da base Satellite do pacote
+str(Satellite) #visualizacao dos dados principais
+
 
 set.seed(7)
-indices <- createDataPartition(Satellite$classes, p=0.8, list=FALSE)
+
+indices <- createDataPartition(Satellite$classes, p=0.8, list=FALSE) #particao em base de treino 80%
 
 treino <- Satellite[indices,]
 
 teste <- Satellite[-indices,]
+
+#RANDOM FOREST
+
+rf <- caret::train(classes~., data=treino, method="rf")
+
+predicoes.rf <- predict(rf, teste)
+
+matriz.rf <- caret::confusionMatrix(predicoes.rf, teste$classes)
+matriz.rf
+
+
+#
