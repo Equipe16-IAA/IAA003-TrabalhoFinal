7ef5fa3a6b687f64bd081c8cc2b1027bafb93b96
igorbuess <igorbuess@gmail.com>
2024-04-17 21:58:38 -0300

    questao2

--- questao2.r
+++ questao2.r
@@ -1,4 +1,4 @@
-# Pacotes carregados abaixo com o comando library foram instalados previamente >
+# Pacotes carregados abaixo com o comando library foram instalados previamente
 
 # Carrega as bibliotecas que serão usadas
 
@@ -13,7 +13,10 @@ library("kernlab")
 dados <- read.csv2("Volumes.csv" , header=T)
 
 # Remove coluna NR
-dados_new = dados[ , 2:5]
+dados_new <- dados[ , 2:5]
+
+# Define uma semente para manter os resultados sempre iguais em cada execução
+set.seed(10)
 
 # Faz a separação entre dados de treinamento e teste
 indices <-createDataPartition(dados_new$VOL,p=0.80, list=FALSE)
@@ -21,17 +24,102 @@ treino <- dados_new[indices,]
 teste <- dados_new[-indices,]
 
 # Treinamento dos modelos
-rf <- train(VOL~.,data=treino,method="rf")
-svm <- train(VOL~.,data=treino,method="svmRadial")
-rna <- train(VOL~.,data=treino,method="nnet")
-
+rf <- caret::train(VOL~.,data=treino,method="rf")
+svm <- caret::train(VOL~.,data=treino,method="svmRadial")
+redeneural <- caret::train(VOL~.,data=treino,method="neuralnet")
+# Modelo alometrico
+alom <- nls(VOL ~ b0 + b1*DAP*DAP*HT, dados_new, start=list(b0=0.5,b1=0.5))
+ 
 # Predições
 predicoes.rf <- predict(rf,teste)
 predicoes.svm <- predict(svm,teste)
-predicoes.rna <- predict(rna,teste)
+predicoes.redeneural <- predict(redeneural,teste)
+predicoes.alom <- predict(alom,teste)
 
-# Comparando resultados
-confusionMatrix(predicoes.rf, teste$VOL)
-confusionMatrix(predicoes.svm, teste$VOL)
-confusionMatrix(predicoes.rna, teste$VOL)
 
+
+#coeficiente de determinação
+
+r2 <- function(valor_real, valor_predito){
+  
+  r2 <- 1 - (sum((valor_real - valor_predito)^2)/sum((valor_real - mean(valor_predito))^2))
+  
+  return(r2)
+}
+
+#erro padrão da estimativa
+
+syx <- function(valor_real, valor_predito){
+  
+  syx <- sqrt(sum((valor_real - valor_predito)^2)/(length(valor_real)-2))
+  
+  return(syx)
+}
+
+#porcentagem do erro padrao
+
+porsyx <- function(valor_real, valor_predito){
+  
+  porsyx <- (syx(valor_real, valor_predito)/mean(valor_real))*100
+  
+  return(porsyx)
+}
+
+
+###comparação entre resultados
+
+#rf
+cat("---------------------RANDOM FOREST---------------------------\n\n")
+cat("R2\n")
+r2(teste$VOL, predicoes.rf)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx)\n")
+syx(teste$VOL, predicoes.rf)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx) percentual\n")
+porsyx(teste$VOL, predicoes.rf)
+cat("\n")
+cat("RMSE\n")
+RMSE(predicoes.rf, teste$VOL)
+
+#svm
+cat("---------------------SVM---------------------------\n\n")
+cat("R2\n")
+r2(teste$VOL, predicoes.svm)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx)\n")
+syx(teste$VOL, predicoes.svm)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx) percentual\n")
+porsyx(teste$VOL, predicoes.svm)
+cat("\n")
+cat("RMSE\n")
+RMSE(predicoes.svm, teste$VOL)
+
+#redes neurais
+cat("---------------------REDES NEURAIS---------------------------\n\n")
+cat("R2\n")
+r2(teste$VOL, predicoes.redeneural)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx)\n")
+syx(teste$VOL, predicoes.redeneural)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx) percentual\n")
+porsyx(teste$VOL, predicoes.redeneural)
+cat("\n")
+cat("RMSE\n")
+RMSE(predicoes.redeneural, teste$VOL)
+
+#modelo alometrico de Spurr
+cat("---------------------ALOMETRICO---------------------------\n\n")
+cat("R2\n")
+r2(teste$VOL, predicoes.alom)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx)\n")
+syx(teste$VOL, predicoes.alom)
+cat("\n")
+cat("Erro Padrão de Estimativa (Syx) percentual\n")
+porsyx(teste$VOL, predicoes.alom)
+cat("\n")
+cat("RMSE\n")
+RMSE(predicoes.alom, teste$VOL)
